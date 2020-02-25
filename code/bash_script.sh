b882c8f176cd4fe9676d7a7063a776b086f69138
cameron arthurs <60911944+cameron-arthurs@users.noreply.github.com>
2020-02-24 23:35:32 -0500

    Update bash_script.sh

--- code/bash_script.sh
+++ code/bash_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# this script is just a test to make sure everything is working with git, this is not the final submitted assignment.
+# this script is just a test to make sure everything is working with git, this is not my final submitted assignment.
 var1="$1"
 
 echo $var1 'assigned to var1'
