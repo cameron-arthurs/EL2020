775d89f4562f7c4d91e8da1e8274c0f5525863bc
cameron-arthurs <arthursc2@newpaltz.hawkmail.edu>
2020-02-26 10:59:21 -0500

    Update bash_script.sh

--- code/bash_script.sh
+++ code/bash_script.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
 
-# this script is just a test to make sure everything is working with git, this is not the final submitted assignment.
 var1="$1"
+if [ $var1 = daynumber ]
+then
+	out=$( date +%j )
+	echo 'Today is day '$out
+	if [ $out -eq 069 ]
+	then
+		echo 'nice'
+	fi
+fi
+if [ $var1 = weekday ]
+then
+	out=$( date +%A )
+	echo 'Today is '$out
+fi
+if [ $var1 = christmas ]
+then
+	today=$( date +%j )
+	if [ ${today:0:-2} = 0 ] #removes 0 digit for days <100 which causes the subtraction result to be off by 10.
+	then
+		today=${today:1}
+	fi
+	let "out = 360 - $today"
+	echo 'There are' $out 'days until Christmas.'
+fi
 
-echo $var1 'assigned to var1'
-echo $var1
+case $var1 in
+	daynumber)
+		;;
+	weekday)
+		;;
+	christmas)
+		;;
+	*)
+		echo 'invaliad argument'
+		echo 'Valid arguements: daynumber, weekday, christmas'
+		;;
+esac
