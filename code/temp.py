60c937b836f8cd09e0ffc22ba1d651b2749204d8
cameron-arthurs <arthursc2@newpaltz.hawkmail.edu>
2020-03-11 15:03:04 -0400

    updated temp.py and added log support in csv format

--- code/temp.py
+++ code/temp.py
@@ -6,15 +6,17 @@ GPIO.setup(26,GPIO.IN)
 GPIO.setup(17,GPIO.OUT)
 
 try:
-	while True:
-		input = GPIO.input(26)
-		if input == True:
-			temp,hum = DHT.read_retry(DHT.DHT11,17)
-			temp = temp * 9/5.0 +32
-			if hum is not None and temp is not None:
-				tempFahr = '{0:0.1f}*F'.format(temp)
-				print('Temperature = {0:0.1f}*F Humidity = {1:0.1f}%'.format(temp, hum))
-			else:
-				print('Failed to get reading.')
+	with open("../log/templog.csv", "a") as log:
+		while True:
+			input = GPIO.input(26)
+			if input == True:
+				temp,hum = DHT.read_retry(DHT.DHT11,17)
+				temp = temp * 9/5.0 +32
+				if hum is not None and temp is not None:
+					tempFahr = '{0:0.1f}*F'.format(temp)
+					print('Temperature = {0:0.1f}*F Humidity = {1:0.1f}%'.format(temp, hum))
+					log.write("{0},{1}\n".format(time.strftime("%Y%m%d %H:%M:%S"),str(tempFahr)))
+				else:
+					print('Failed to get reading.')
 except KeyboardInterrupt:
 	GPIO.cleanup()
